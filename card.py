5729ed7a418c8d40908dbd931c5edf67aaa22dfe
JaneWay <nkuborn@gmail.com>
2018-11-19 14:58:20 +0100

    first commit

--- card.py
+++ card.py
@@ -7,30 +7,30 @@ class Card:
     """
     
     def __init__(self, suit, number):
-    """
-    this is the constructor with two properties
-    """
+        """
+        this is the constructor with two properties
+        """
         self._suit = suit
         self._number = number
 
     def __repr__(self):
-    """
-    overwriting to string
-    """
+        """
+        overwriting to string
+        """
         return self.number + " of " + self.suit
     
     @property
     def suit(self):
-    """
-    gets the property suit"
-    """
+        """
+        gets the property suit
+        """
         return self._suit
     
     @suit.setter
-    """
-    sets the property suit
-    """
     def suit(self, suit):
+        """
+        sets the property suit
+        """
         if suit in ["hearts", "clubs", "diamonds", "spades"]:
             self._suit = suit
         else:
@@ -38,16 +38,16 @@ class Card:
     
     @property
     def number(self):
-    """
-    gets the property number
-    """
+        """
+        gets the property number
+        """
         return self._number
     
     @number.setter
-    """
-    sets the number values
-    """
     def number(self, number):
+        """
+        sets the number values
+        """
         if number in ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K","A"]:
             self._number = number
         else:
--- deck.py
+++ deck.py
@@ -8,17 +8,17 @@ class Deck:
     """
     
     def __init__(self):
-    """
-    this is the constructor
-    """
+        """
+        this is the constructor
+        """
         self.cards = []
         self.populate()
         print(self._cards)
 
     def populate(self):
-    """
-    sets a list/array
-    """
+        """
+        sets a list/array
+        """
         suits = ["hearts", "clubs", "diamonds", "spades"]
         numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K","A"]
         self._cards = [ Card(s, n) for s in suits for n in numbers]
